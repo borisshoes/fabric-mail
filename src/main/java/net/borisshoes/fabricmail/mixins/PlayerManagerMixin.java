76ffd362a3f9273e8c4f72b1cdfe6151259dcb34
borisshoes <borisshoeslin@yahoo.com>
2024-01-31 12:32:41 -0500

    Fixed fatal bug

--- src/main/java/net/borisshoes/fabricmail/mixins/PlayerManagerMixin.java
+++ src/main/java/net/borisshoes/fabricmail/mixins/PlayerManagerMixin.java
@@ -3,6 +3,7 @@ package net.borisshoes.fabricmail.mixins;
 import net.borisshoes.fabricmail.FabricMail;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ConnectedClientData;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.stat.Stats;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(PlayerManager.class)
 public class PlayerManagerMixin {
    @Inject(at = @At(value = "TAIL"), method = "onPlayerConnect")
-   private void onPlayerJoin(ClientConnection connection, ServerPlayerEntity player, CallbackInfo info) {
+   private void onPlayerJoin(ClientConnection connection, ServerPlayerEntity player, ConnectedClientData clientData, CallbackInfo ci) {
       FabricMail.informMail(player);
    }
 }
